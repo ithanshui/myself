6f105efc1a57bfac565b3c0640dbc2031ff30f22
lsbrucelincoln <276505128@qq.com>
2016-03-16 16:52:08 +0800

    firstPage and part of secondPage

--- app/scripts/fullPageConfig.js
+++ app/scripts/fullPageConfig.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({paddingTop: '50px', anchors:['firstPage', 'secondPage', 'thirdPage','fourthPage','fifthPage'],menu: '#myMenu'});
+    $('#fullpage').fullpage({paddingTop: '50px', anchors:['firstPage', 'secondPage', 'thirdPage','fourthPage'],menu: '#myMenu'});
 });
